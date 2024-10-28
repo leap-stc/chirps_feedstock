a903a3be31bda68fdd0619b272c375aa140f386c
Julius Busecke <julius@ldeo.columbia.edu>
2024-10-28 12:42:04 -0400

    Update config_dataflow.py

--- configs/config_dataflow.py
+++ configs/config_dataflow.py
@@ -7,8 +7,9 @@ FEEDSTOCK_NAME = repo_path.split('/')[-1]
 c.Bake.prune = 0
 c.Bake.bakery_class = "pangeo_forge_runner.bakery.dataflow.DataflowBakery"
 c.Bake.container_image = "quay.io/leap-stc/rclone-beam:2024.09.24"
-c.DataflowBakery.use_dataflow_prime = True
-c.DataflowBakery.max_workers = 50
+c.DataflowBakery.use_dataflow_prime = False
+c.DataflowBakery.machine_type = "n2-highcpu-4"
+c.DataflowBakery.max_workers = 20
 c.DataflowBakery.use_public_ips = True
 c.DataflowBakery.service_account_email = (
     "leap-community-bakery@leap-pangeo.iam.gserviceaccount.com"
