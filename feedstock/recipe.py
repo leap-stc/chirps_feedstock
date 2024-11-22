c38ea2bbd21035e769c5eaf903291d264cddd797
Raphael Hagen <norlandrhagen@gmail.com>
2024-11-22 16:36:26 -0700

    typo

--- feedstock/recipe.py
+++ feedstock/recipe.py
@@ -44,7 +44,7 @@ pattern_a = pattern_from_file_sequence(input_urls, concat_dim="time")
 recipe = (
     beam.Create(pattern_a.items())
     | OpenURLWithFSSpec(fsspec_sync_patch=True)
-    | OpenWithXarray(load=True, xarray_open_kwargs={'engine':'h5netrcdf'})
+    | OpenWithXarray(load=True, xarray_open_kwargs={'engine':'h5netcdf'})
     | StoreToZarr(
         store_name="chirps-global-daily.zarr",
         # FIXME: This is brittle. it needs to be named exactly like in meta.yaml...
